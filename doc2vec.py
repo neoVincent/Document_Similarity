a1e296ccd89af7ad5fb287938ff203af185b70f8
Kaiyi <neovincent@163.com>
2019-12-03 17:28:24 -0500

    Fix diff python version prob

--- doc2vec.py
+++ doc2vec.py
@@ -3,30 +3,41 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf, SparkContext
 import os
 
-# Spark connect mysql
-url = "jdbc:mysql://localhost:3306/moviereview"
-doctb = "document"
-costb = "similarity"
-user = "root"
-pwd = "123456"
-driver = "com.mysql.jdbc.Driver"
 
+# set up
 SUBMIT_ARGS = "--jars mysql-connector-java-8.0.18.jar pyspark-shell"
 os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/local/bin/python3"
+
 conf = SparkConf()
 context = SparkContext(conf=conf)
 spark = SparkSession(context)
 
-mysqldf = spark.read.format("jdbc").options(
-    url=url,
-    driver=driver,
-    dbtable=doctb,
-    user=user,
-    password=pwd).load()
+# Spark connect mysql
+user = "root"
+pwd = "123456"
+driver = "com.mysql.jdbc.Driver"
+jdbcHostname = "localhost"
+jdbcDatabase = "moviereview"
+jdbcPort = 3306
+
+jdbcUrl = "jdbc:mysql://{0}:{1}/{2}".format(jdbcHostname, jdbcPort, jdbcDatabase)
+connectionProperties = {
+  "user" : user,
+  "password" : pwd,
+  "driver" : driver
+}
+
+docdf = spark.read.jdbc(url=jdbcUrl, table="document", properties=connectionProperties)
+docdf.show(n=2)
+
+# computer vector
+comments = docdf.rdd.map(lambda r: [r[1]]).collect()
+for c in comments:
+    print(c)
 
 
-
-docdf = spark.read.jdbc()
 
 spark.stop()
  # You could use ndarray.dumps() to pickle it to a string then write it to a BLOB field? Recover it using numpy.loads()
