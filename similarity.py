171d2d4fa627aaec4b22526c2bc8030a2c9b9508
Kaiyi <neovincent@163.com>
2019-12-03 23:17:16 -0500

    Add logic for similarity

---
+++ similarity.py
@@ -0,0 +1,14 @@
+# Given an docId, find the top k similar document in the database
+
+
+# DEBUG
+docId = 1
+K = 2
+
+# Connect to db get the document full text by id
+
+# Calculate the doc vector
+
+# Spark key value map, reduce by key (key is cosine)
+
+# return the document id or text
